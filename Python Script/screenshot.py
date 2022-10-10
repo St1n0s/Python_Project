5345b78075f4d3def50aecfd30d8737acb66ab3e
St1n0s <90309125+St1n0s@users.noreply.github.com>
2022-10-10 15:46:28 +0200

    update

---
+++ Python Script/screenshot.py
@@ -0,0 +1,4 @@
+import pyautogui # Hier import hij een API??
+
+myScreenshot = pyautogui.screenshot() # Hier maakt hij de screenshot
+myScreenshot.save(r'C:/Applications/MAMP/htdocs/Python_Project/') # Hier slaat hij de screenschot op in de juiste map
